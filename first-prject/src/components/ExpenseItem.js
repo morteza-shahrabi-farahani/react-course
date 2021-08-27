ff8c8f61f42e69c733a01236b762030bfa7dc750
morteza shahrabi farahani <morteza.shahrabii@gmail.com>
2021-08-27 15:36:55 +0430

    adding styles

--- first-prject/src/components/ExpenseItem.js
+++ first-prject/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>hello world!</h2>;
+  return (
+    <div className="expense-item">
+      <div>September 23th 2000</div>
+      <div className="expense-item__description">
+        <h2>First project</h2>
+        <div className="expense-item__price">$777</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
