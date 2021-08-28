9da9fb6f567c2051482d63049b1d3bf5bc30ca64
morteza shahrabi farahani <morteza.shahrabii@gmail.com>
2021-08-28 15:05:54 +0430

    adding form inputs

---
+++ first-prject/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpnse = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm></ExpenseForm>
+    </div>
+  );
+};
+
+export default NewExpnse;
