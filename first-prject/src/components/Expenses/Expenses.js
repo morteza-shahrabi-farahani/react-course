aca8d29c83ab5c4e63c32155a79b9229e75c63c8
morteza shahrabi farahani <morteza.shahrabii@gmail.com>
2021-08-29 16:17:11 +0430

    adding filter for year

--- first-prject/src/components/Expenses/Expenses.js
+++ first-prject/src/components/Expenses/Expenses.js
@@ -9,13 +9,17 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       ></ExpensesFilter>
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
