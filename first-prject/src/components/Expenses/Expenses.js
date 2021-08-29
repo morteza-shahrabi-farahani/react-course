2aa1e6805ae9cbbdf2213707646ed3df372219f5
morteza shahrabi farahani <morteza.shahrabii@gmail.com>
2021-08-29 16:06:25 +0430

    now we can add item to the list

--- first-prject/src/components/Expenses/Expenses.js
+++ first-prject/src/components/Expenses/Expenses.js
@@ -17,6 +17,7 @@ function Expenses(props) {
       ></ExpensesFilter>
       {props.items.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
